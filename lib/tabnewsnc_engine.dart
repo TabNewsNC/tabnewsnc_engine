43fb760a4eead9deed927d95070f55b73de82432
João Sereia <joao.lo.sereia@gmail.com>
2025-11-29 17:07:52 -0300

    chore(analysis_options): add very_good_analysis

--- lib/tabnewsnc_engine.dart
+++ lib/tabnewsnc_engine.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// TabNewsNC Engine
 library;
-
-export 'src/tabnewsnc_engine_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- test/tabnewsnc_engine_test.dart
+++ test/tabnewsnc_engine_test.dart
@@ -1,16 +1,7 @@
-import 'package:tabnewsnc_engine/tabnewsnc_engine.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    test('First Test', () {});
   });
 }
